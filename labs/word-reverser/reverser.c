adc70d88c048be8a8e8cb5f4b111545d28074db6
Santiago Yeomans <santiagoyeomansmolina@gmail.com>
2021-02-24 17:56:11 -0700

    Word reverses lab solved

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
 
-int main(){
-    // Place your magic here
+#define IN   1   /* inside a word */
+#define OUT  0   /* outside a word */
+
+/* count lines, words, and characters in input */
+
+int main()
+
+{
+    int i, state, pos;
+    char c, word[100];
+    //state = OUT;
+
+while ((c = getchar()) != EOF) {    
+	if (c == ' ' || c == '\n' || c == '\t') {
+      //REVERSE
+       for(int i =pos; i >= 1; i--){
+        printf("%c", word[i]);
+      }
+       printf("%s", " ");
+
+      pos = 0;
+    }
+  else {
+    pos++;
+	  word[pos] = c;
+    pos++;
+	}
+    }
+
+   printf("%s", " \n ");
     return 0;
 }
