90fcf82d58b5944a23da5ea3a8af0ec76c688450
Santiago Yeomans <santiagoyeomansmolina@gmail.com>
2021-03-23 00:02:27 -0700

    Solve str library

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -1,5 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h> 
+#include <stdbool.h>
 
-int main() {
-    return 0;
+int mystrlen(char *str);
+char *mystradd(char *origin, char *addition);
+int mystrfind(char *origin, char *substr);
+
+int main(int argc, char **argv) {
+    if(strcmp(argv[1], "-add") == 0){
+        if(argc != 4){
+            printf("Failed adition\n");
+            exit(0);
+        }
+        
+        printf("Initial Lenght      : %d\n", mystrlen(argv[2]));
+        char *newString = mystradd(argv[2], argv[3]);
+        printf("New String          : %s\n", newString );
+        printf("New String          : %d\n", mystrlen(newString));
+
+
+    }else if(strcmp(argv[1], "-find") == 0){
+        if(argc != 4){
+            printf("Failed find\n");
+            exit(0);
+        }
+    
+        int pos = mystrfind(argv[2], argv[3]);
+        printf("[%s] string was found at [%d] position\n", argv[3], pos);
+
+    }else{
+        printf("Input Error");
+        exit(0);
+    }
 }
+
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,68 @@
 
 
 int mystrlen(char *str){
-    return 0;
+    int count = 0;
+    char * p;
+    
+    for (p = str; *p != '\0'; p++) {
+        count++;
+    }
+
+    return count;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    char *newStr = origin;
+    char *end = addition;
+    int i = 0;
+
+    while(*newStr != '\0'){
+        newStr++;
+    }   
+
+    while(*end != '\0'){
+        *newStr++ = *end++;
+    }
+
+    *newStr = '\0';
+
+    return origin;
+    
+
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+    int pos = 0;
+    int len = mystrlen(substr);
+    int originPos = 0;
+    int subPos = 0;
+
+    while(origin[originPos] != '\0') {
+        pos = originPos;
+
+
+        while(origin[originPos] != '\0' && origin[originPos]==substr[subPos] ){
+            subPos++;
+            originPos++;
+        }
+
+        if(subPos == len){
+            return pos;
+
+        }else{
+            subPos = 0;
+            originPos++;   
+            
+        }
+
+        
+        
+    }
+
+
+    return -1;
 }
+
+
+
+
